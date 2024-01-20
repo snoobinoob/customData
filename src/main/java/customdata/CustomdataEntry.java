926955c10b02d2a0e9a6735e7a72ff031950ed51
Snoobinoob <cheeseycheese99@gmail.com>
2024-01-20 11:57:08 -0500

    Initial commit

---
+++ src/main/java/customdata/CustomdataEntry.java
@@ -0,0 +1,10 @@
+package customdata;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class CustomdataEntry {
+    public void init() {
+        System.out.println("Hello world from Custom Data!");
+    }
+}
